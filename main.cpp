7cb07f18f1906e4ebb995bd1775127b98609eefb
ShotaIuchi <shotaiuchi.develop@gmail.com>
2022-12-14 21:35:09 +0900

    ikutame

--- main.cpp
+++ main.cpp
@@ -14,9 +14,108 @@
 #include <SDL_opengl.h>
 #endif
 
+#include <iostream>
+#include <string>
+#include <map>
+
+class Window
+{
+public:
+    virtual ~Window() = default;
+    void setShow(bool isShow)
+    {
+        this->mIsShow = isShow;
+    }
+    bool isShow() const
+    {
+        return this->mIsShow;
+    }
+    void draw()
+    {
+        if (isShow())
+        {
+            this->onDraw();
+        }
+    }
+    virtual void onDraw() = 0;
+
+private:
+    bool mIsShow = false;
+};
+
+std::map<std::string, Window *> WINDOW_LIST{};
+
+class Context final
+{
+public:
+    void doOutput(const std::string &arg)
+    {
+        std::cout << "output:" << arg << std::endl;
+    }
+};
+
+class WindowA : public Window
+{
+public:
+    WindowA(Context &context)
+        : mContext(context) {}
+    virtual ~WindowA() = default;
+    virtual void onDraw()
+    {
+        ImGui::Begin("WindowA");
+        ImGui::Text("This is some useful text.");
+        if (ImGui::Button("show window b"))
+        {
+            this->setShow(false);
+            WINDOW_LIST.at("win-b")->setShow(true);
+        }
+        if (ImGui::Button("output"))
+        {
+            this->mContext.doOutput("win-a");
+        }
+        ImGui::SameLine();
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+
+private:
+    Context mContext;
+};
+
+class WindowB : public Window
+{
+public:
+    WindowB(Context &context)
+        : mContext(context) {}
+    virtual ~WindowB() = default;
+    virtual void onDraw()
+    {
+        ImGui::Begin("WindowB");
+        if (ImGui::Button("show window A"))
+        {
+            this->setShow(false);
+            WINDOW_LIST.at("win-a")->setShow(true);
+        }
+        if (ImGui::Button("output"))
+        {
+            this->mContext.doOutput("win-b");
+        }
+        ImGui::SameLine();
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+
+private:
+    Context mContext;
+};
+
 // Main code
 int main(int, char **)
 {
+    Context mainContext{};
+    WINDOW_LIST.emplace("win-a", new WindowA(mainContext));
+    WINDOW_LIST.emplace("win-b", new WindowB(mainContext));
+
     // Setup SDL
     // (Some versions of SDL before <2.0.10 appears to have performance/stalling issues on a minority of Windows systems,
     // depending on whether SDL_INIT_GAMECONTROLLER is enabled or disabled.. updating to the latest version of SDL is recommended!)
@@ -97,6 +196,8 @@ int main(int, char **)
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    WINDOW_LIST.at("win-a")->setShow(true);
+
     // Main loop
     bool done = false;
     while (!done)
@@ -121,42 +222,47 @@ int main(int, char **)
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
+        for (auto &window : WINDOW_LIST)
+        {
+            window.second->draw();
+        }
+
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        // if (show_demo_window)
+        //     ImGui::ShowDemoWindow(&show_demo_window);
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
+        // // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        // {
+        //     static float f = 0.0f;
+        //     static int counter = 0;
 
-            ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+        //     ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
+        //     ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
+        //     ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+        //     ImGui::Checkbox("Another Window", &show_another_window);
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+        //     ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
+        //     ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
 
-            if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+        //     if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+        //         counter++;
+        //     ImGui::SameLine();
+        //     ImGui::Text("counter = %d", counter);
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
+        //     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        //     ImGui::End();
+        // }
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
+        // // 3. Show another simple window.
+        // if (show_another_window)
+        // {
+        //     ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        //     ImGui::Text("Hello from another window!");
+        //     if (ImGui::Button("Close Me"))
+        //         show_another_window = false;
+        //     ImGui::End();
+        // }
 
         // Rendering
         ImGui::Render();
